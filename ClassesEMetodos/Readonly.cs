0aa683826e8b7de5afdfa72020b8543fc64d5833
Enio Zavattaro <eniozavat@gmail.com>
2022-07-20 10:43:18 -0300

    exercícios classes e métodos

---
+++ ClassesEMetodos/Readonly.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CursoCSharp.ClassesEMetodos
+{
+    class Readonly
+    {
+        public static void Executar() //visível(public) - diretamente associada a classe(static) - nao retorna nada(void)
+        {
+
+        }
+    }
+}
